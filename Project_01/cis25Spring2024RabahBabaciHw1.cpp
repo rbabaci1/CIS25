7ab55db408d99501827a828fad331fc1819720fe
rabah babaci <4rabah@gmail.com>
2024-02-09 15:11:04 -0800

    initial commit - homework#1

---
+++ Project_01/cis25Spring2024RabahBabaciHw1.cpp
@@ -0,0 +1,91 @@
+/**
+ * Program Name: cis25Spring2024RabahBabaciHw1.cpp
+ * Discussion:   Simple C++
+ * Written By:   R. Babaci
+ * Date:         2024/02/01
+ */
+
+
+// Include/Header Files
+#include <iostream>
+using namespace std;
+
+// Application Driver
+int main() {
+    cout << "We write code to manipulate data (which are provided by the user)\n"
+    "to produce the required outcome in the most efficient way!\n\n"
+
+    "CIS 25 - C++ Programming\n"
+    "Laney College\n"
+    "Rabah Babaci\n\n"
+
+    "Information --\n"
+    "  Assignment:               HW #1 Exercise #1\n"
+    "  Implemented by:           Rabah Babaci\n"
+    "  Required Submission Date: 2024/02/10\n"
+    "  Actual Submission Date:   2024/02/09\n\n";
+
+    cout << "My name is Rabah Babaci!\n\n"
+
+    "My initials are patterned as below.\n\n";
+
+    // Printing the initials
+    cout << "RRRRRRRRR      BBBBBBBBB\n"
+            "R        R     B        B\n"
+            "R        R     B        B\n"
+            "R         R    B         B\n"
+            "R        R     B        B\n"
+            "RRRRRRRRR      BBBBBBBBB\n"
+            "R  R           B        B\n"
+            "R   R          B        B\n"
+            "R     R        B         B\n"
+            "R       R      B        B\n"
+            "R         R    BBBBBBBBB\n\n";
+
+    // Printing the character counts
+    cout << "There are 36 R’s, and 43 B’s.\n\n"
+    "There are 79 characters in total." << endl;
+
+    return 0;
+}
+
+/** PROGRAM_OUTPUT
+We write code to manipulate data (which are provided by the user)
+to produce the required outcome in the most efficient way!
+
+CIS 25 - C++ Programming
+Laney College
+Rabah Babaci
+
+Information --
+  Assignment:               HW #1 Exercise #1
+  Implemented by:           Rabah Babaci
+  Required Submission Date: 2024/02/10
+  Actual Submission Date:   2024/02/09
+
+My name is Rabah Babaci!
+
+My initials are patterned as below.
+
+RRRRRRRRR      BBBBBBBBB
+R        R     B        B
+R        R     B        B
+R         R    B         B
+R        R     B        B
+RRRRRRRRR      BBBBBBBBB
+R  R           B        B
+R   R          B        B
+R     R        B         B
+R       R      B        B
+R         R    BBBBBBBBB
+
+There are 36 R’s, and 43 B’s.
+
+There are 79 characters in total.
+*/
+
+
+
+/** Logic_Code_Output_Issues
+
+*/ 
