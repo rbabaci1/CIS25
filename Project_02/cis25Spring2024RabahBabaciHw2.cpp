bfc9c8f471b113ec85e3f6759cb0843a9d5eb4e4
rabah babaci <4rabah@gmail.com>
2024-02-22 19:11:33 -0800

    implement homework #2

--- Project_02/cis25Spring2024RabahBabaciHw2.cpp
+++ Project_02/cis25Spring2024RabahBabaciHw2.cpp
@@ -1,109 +1,139 @@
 /**
- * Program Name: cis25Spring2024RabahBabaciHw1.cpp
- * Discussion:   HW #1
+ * Program Name: cis25Spring2024RabahBabaciHw2.cpp
+ * Discussion:   HW #2
  * Written By:   R. Babaci
- * Date:         2024/02/09
+ * Date:         2024/02/24
  */
 
 // Include/Header Files
 #include <iostream>
 using namespace std;
 
-// // Application Driver
-// int main() {
-    // cout << "We write code to manipulate data (which are provided by the user)\n"
-    // "to produce the required outcome in the most efficient way!\n\n"
-
-    // "CIS 25 - C++ Programming\n"
-    // "Laney College\n"
-    // "Rabah Babaci\n\n"
-
-    // "Information --\n"
-    // "  Assignment:               HW #1 Exercise #1\n"
-    // "  Implemented by:           Rabah Babaci\n"
-    // "  Required Submission Date: 2024/02/10\n"
-    // "  Actual Submission Date:   2024/02/09\n\n";
-
-//     // !!! Follow the rest of the steps of the homework
-
-//     return 0;
-// }
 
 // Function to check if a number is positive, negative, or zero
 string checkSign(int number) {
-    if (number > 0) {
-        return "positive";
-    } else if (number < 0) {
-        return "negative";
-    } else {
-        return "zero";
-    }
+    return (number > 0) ? "positive" : (number < 0) ? "negative" : "zero";
 }
 
 // Function to check if a number is even or odd
 string checkParity(int number) {
-    if (number == 0) {
-        // Technically, zero is even, but you might want special handling
-        return "even (zero)";
-    } else if (number % 2 == 0) {
-        return "even";
-    } else {
-        return "odd";
-    }
+    return (number % 2) ? "odd" : "even";
 }
 
-int main() {
-    int num1;
-    int num2;
+// Function to perform and display arithmetic
+void performArithmeticAndDisplay(int num1, int num2) {
+    cout << "(" << num1 << ") + (" << num2 << ") --> " << (num1 + num2) << "\n"
+         << "(" << num1 << ") - (" << num2 << ") --> " << (num1 - num2) << "\n"
+         << "(" << num1 << ") * (" << num2 << ") --> " << (num1 * num2) << "\n"
+         << "(" << num1 << ") / (" << num2 << ") --> " << (num1 / num2) << "\n"
+         << "(" << num1 << ") % (" << num2 << ") --> " << (num1 % num2) << endl;
+}
 
-    cout << "We write code to manipulate data (which are provided by the user)\n"
-    "to produce the required outcome in the most efficient way!\n\n"
+void printHeader() {
+    cout << "We write code to manipulate data (which are\n"
+    "provided by the user) to produce the\n"
+    "required outcome in the most efficient way!\n\n"
 
     "CIS 25 - C++ Programming\n"
     "Laney College\n"
     "Rabah Babaci\n\n"
 
     "Information --\n"
-    "  Assignment:               HW #1 Exercise #1\n"
+    "  Assignment:               HW #2\n"
     "  Implemented by:           Rabah Babaci\n"
     "  Required Submission Date: 2024/02/10\n"
-    "  Actual Submission Date:   2024/02/09\n\n";
+    "  Actual Submission Date:   2024/02/09\n";
+}
+
+void processIntegers() {
+    int num1;
+    int num2;
 
     // Input for the first integer
-    cout << "Enter an integer #1: ";
+    cout << "\nEnter an integer #1: ";
     cin >> num1;
-    cout << num1 << " is " << checkSign(num1) << " and " << checkParity(num1) << "!" << endl;
+    cout << "\n  " << num1 << " is " << checkSign(num1) << " and " << checkParity(num1) << "!" << endl;
 
     // Input for the second integer
-    cout << "Enter an integer #2: ";
+    cout << "\nEnter an integer #2: ";
     cin >> num2;
-    cout << num2 << " is " << checkSign(num2) << " and " << checkParity(num2) << "!" << endl;
+    cout << "\n  " << num2 << " is " << checkSign(num2) << " and " << checkParity(num2) << "!\n" << endl;
 
     // Performing and displaying arithmetic operations
-    cout << "(" << num1 << ") + (" << num2 << ") --> " << (num1 + num2) << endl;
-    cout << "(" << num1 << ") – (" << num2 << ") --> " << (num1 - num2) << endl;
-    cout << "(" << num1 << ") * (" << num2 << ") --> " << (num1 * num2) << endl;
+    performArithmeticAndDisplay(num1, num2);
+}
 
-    // Division operation with check to prevent division by zero
-    if (num2 != 0) {
-        cout << "(" << num1 << ") / (" << num2 << ") --> " << (num1 / num2) << endl;
-        cout << "(" << num1 << ") % (" << num2 << ") --> " << (num1 % num2) << endl;
-    } else {
-        cout << "Cannot perform division or modulo by zero." << endl;
-    }
+// Application Driver
+int main() {
+    // This function will print the header
+    printHeader();
 
-    cout << endl; // Adds an extra line for spacing
-
-    // !!! repeat as shown in homework description
+    // This function will process integers
+    processIntegers();
+    processIntegers();
+    processIntegers();
 
     return 0;
 }
 
 
 /** PROGRAM_OUTPUT
+We write code to manipulate data (which are
+provided by the user) to produce the
+required outcome in the most efficient way!
 
+CIS 25 - C++ Programming
+Laney College
+Rabah Babaci
+
+Information --
+  Assignment:               HW #2
+  Implemented by:           Rabah Babaci
+  Required Submission Date: 2024/02/25
+  Actual Submission Date:   2024/02/24
+
+Enter an integer #1: 24
+    
+  24 is positive and even!
+
+Enter an integer #2: 7
+
+  7 is positive and odd!
+
+(24) + (7) --> 31
+(24) - (7) --> 17
+(24) * (7) --> 168
+(24) / (7) --> 3
+(24) % (7) --> 3
+
+Enter an integer #1: 12
+
+  12 is positive and even!
+
+Enter an integer #2: 365
+
+  365 is positive and odd!
+
+(12) + (365) --> 377
+(12) - (365) --> -353
+(12) * (365) --> 4380
+(12) / (365) --> 0
+(12) % (365) --> 12
+
+Enter an integer #1: 12
+
+  12 is positive and even!
+
+Enter an integer #2: -365
+
+  -365 is negative and odd!
+
+(12) + (-365) --> -353
+(12) - (-365) --> 377
+(12) * (-365) --> -4380
+(12) / (-365) --> 0
+(12) % (-365) --> 12
 */
-
 
 
 /** Logic_Code_Output_Issues
